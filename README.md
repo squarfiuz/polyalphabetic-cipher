dd8420870735a1e1e2242ad5c8c1f476c07bc91e
squarfiuz <squarfiuz@gmail.com>
2022-09-21 19:32:06 +0200

    Add decrypt fonction and readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Polyalphabetic Cipher
+
+## How to use?
+
+Type `python main.py` to run the program.
+
+First enter whether you want to encrypt or decrypt a message by typing `encrypt` or `decrypt`.
+
+Then enter the message to be encrypted/decrypted.
+
+Finally give the key that will allow you to encrypt/decrypt the message.
+
+The program will give you the new encrypted/decrypted word.
+
+<img alt="Example" src="./example.png">
--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-message = str(input("What message do you want to encrypt? ")).replace(" ", "")
-key = str(input("With which key do you want to encrypt this message? ")).replace(" ", "")
+choice = str(input("Do you want encrypt or decrypt? "))
+if choice != "encrypt" and choice != "decrypt":
+    quit()
+message = str(input("What message do you want to encrypt/decrypt? ")).replace(" ", "")
+key = str(input("With which key do you want to encrypt/decrypt this message? ")).replace(" ", "")
 
 key_index = 0
-encrypt_message = ""
-alphabet = list("abcdefghijklmnopqrstuvwxyz")
+outpout_message = ""
+alphabet = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 for letter in message:
-    encrypt_letter_number = (alphabet.index(letter) + 1) + (alphabet.index(key[key_index]) + 1)
-    encrypt_letter_number_format = 0
-    if encrypt_letter_number > 26:
-        encrypt_letter_number_format = encrypt_letter_number - 26
+    letter_number_format = 0
+    if choice == "encrypt":
+        letter_number = (alphabet.index(letter) + 1) + (alphabet.index(key[key_index]) + 1)
+
+
+        if letter_number > 52:
+            letter_number_format = letter_number - 52
+        else:
+            letter_number_format = letter_number
     else:
-        encrypt_letter_number_format = encrypt_letter_number
-    encrypt_letter = alphabet[encrypt_letter_number_format - 1]
-    encrypt_message = encrypt_message + encrypt_letter
+        letter_number = (alphabet.index(letter) + 1) - (alphabet.index(key[key_index]) + 1)
+
+        if letter_number < 1:
+            letter_number_format = letter_number + 52
+        else:
+            letter_number_format = letter_number
+
+    outpout_letter = alphabet[letter_number_format - 1]
+    outpout_message = outpout_message + outpout_letter
     if key_index >= len(key) - 1:
         key_index = 0
     else:
         key_index = key_index + 1
 
-print("Your encrypt message is:", encrypt_message)
+print("Your encrypt message is:", outpout_message)
